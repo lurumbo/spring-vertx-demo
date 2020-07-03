335be05fdac5a79c72cb8403d93bc926e7b35045
Lucia Rumbo <lurumbo18@gmail.com>
2020-07-03 09:41:30 -0300

    Initial commit

---
+++ src/main/java/com/pigmalion/springvertxdemo/SpringVertxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pigmalion.springvertxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVertxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVertxDemoApplication.class, args);
+	}
+
+}
